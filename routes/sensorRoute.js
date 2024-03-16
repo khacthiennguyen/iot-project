ea303b3d5f3f7a8d3376c73b23fad091e9a4e462
your name <khacthien.brand@gmail.com>
2024-03-16 19:18:35 +0700

    push code context

--- routes/sensorRoute.js
+++ routes/sensorRoute.js
@@ -1,30 +1,33 @@
 module.exports = app => {
-    const sensor = require("../controllers/sensorController");
-  
-    var router = require("express").Router();
-  
-    // Tạo mới một đối tượng cảm biến
-    router.post("/", sensor.create);
-  
-    // Retrieve all Cambien
-    router.get("/", sensor.findAll);
-  
-    // Retrieve a single Cambien with id
-    router.get("/:id", sensor.findOne);
-  
-    // Update a Cambien with id
-    router.put("/:id", sensor.update);
-  
-    // Delete a Cambien with id
-    router.delete("/:id", sensor.delete);
-  
-    // Delete all Cảm biến
-    router.delete("/", sensor.deleteAll);
-  
+  const sensor = require("../controllers/sensorController");
+
+  var router = require("express").Router();
+
+  // Tạo mới một đối tượng cảm biến
+  router.post("/", sensor.create);
+
+  // Retrieve all Cambien
+  router.get("/", sensor.findAll);
+
+  // Retrieve all Cambien activate
+  router.get("/activate", sensor.findAllActivate);
+
+  // Retrieve a single Cambien with id
+  router.get("/:id", sensor.findOne);
+
+  // Update a Cambien with id
+  router.put("/:id", sensor.update);
+
+  // Delete a Cambien with id
+  router.delete("/:id", sensor.delete);
+
+  // Delete all Cảm biến
+  router.delete("/", sensor.deleteAll);
 
 
 
 
-    
-    app.use('/api/sensor', router);
-  }
+
+
+  app.use('/api/sensor', router);
+}
